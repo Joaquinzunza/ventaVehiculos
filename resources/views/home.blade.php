053072414e6470444ab1b16da3c5d51591a8f550
joaquinzunza <joaquinzunza@gmail.com>
2015-04-08 23:25:09 -0600

    Encabezado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
-
+					<h1> HOLA </h1>
 				<div class="panel-body">
 					You are logged in!
 				</div>
